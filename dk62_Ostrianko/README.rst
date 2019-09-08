72e778b6a02098512b091aaad09faf7207f31566
Alex Ostrianko <sasha_ostrianko@mail.ru>
2019-09-08 15:25:35 +0300

    Squashed all editing
    Update README.rst

---
+++ dk62_Ostrianko/README.rst
@@ -0,0 +1,9 @@
+Репозиторій для лабораторних робіт з курсу Embedded Linux
+===================================================
+
+Контактні дані:
+---------------
+
+* **Ім'я:** Острянко Олександр
+* **Telegram:** `@AlexOstrianko <https://tg.me/AlexOstrianko>`__
+* **Email:** `sasha_ostrianko@mail.ru <mailto:sasha_ostrianko@mmail.ru>`_
--- dk62_Ostrianko/lab1_first_embedded_app/src/thread.c
+++ dk62_Ostrianko/lab1_first_embedded_app/src/thread.c
@@ -1,25 +1,27 @@
+#include "func.h"
 #include <pthread.h>
 #include <stdio.h>
 #include <time.h>
-#include "../inc/header.h"
 
 
-int main()
+int main(void)
 {
+	global_counter = 0;
+	
 	pthread_t first_thread, second_thread;
 	
 	clock_t start = clock();
 
-	pthread_create(&first_thread, NULL, func, NULL);
-	pthread_create(&second_thread, NULL, func, NULL);
+	pthread_create(&first_thread, NULL, &func, NULL);
+	pthread_create(&second_thread, NULL, &func, NULL);
 	pthread_join(first_thread, NULL);
 	pthread_join(second_thread, NULL);
 	
 	clock_t end = clock();
 
 	printf("Program counted to %i\n", global_counter);
-	printf("Program ended for %f seconds.\n", (
-	double)(end - start) / CLOCKS_PER_SEC);
+	printf("Program ended for %f seconds.\n",
+			(double)(end - start) / CLOCKS_PER_SEC);
 
-	return 0;	
+	return 0;
 }
