9b33fd29b01b74c212ad9fdef5aa805ccb317e9a
JamsAurom <maksimaldokhin84@gmail.com>
2019-09-10 21:34:25 +0300

    Add files via upload

--- dk61_aldokhin/lab1_first_embedded_app/src/main.c
+++ dk61_aldokhin/lab1_first_embedded_app/src/main.c
@@ -4,33 +4,36 @@
 
 int count;
 int atoi(const char *nptr);
-void *potok(void *param);
+void *stream_iter(void *param);
 
-int iter = 0;
+int iter = 0; // constant used for iteration and testing
 
 int main(int argc, char *argv[])
 {
-     clock_t c_s, c_e;
-     c_s = clock();
+        clock_t c_s, c_e; // variables for calculating program execution speed
+        c_s = clock();
 
-     pthread_t tid0;
-     pthread_t tid1;
+        pthread_t tid0; // stream creation
+        pthread_t tid1;
 
-     pthread_create(&tid0, NULL, potok, NULL);
-     pthread_create(&tid1, NULL, potok, NULL);
-     pthread_join(tid0, NULL);
-     pthread_join(tid1, NULL);
+        pthread_create(&tid0, NULL, stream_iter, NULL); // call thread
+        pthread_create(&tid1, NULL, stream_iter, NULL);
+        pthread_join(tid0, NULL); // waiting for thread to finish
+        pthread_join(tid1, NULL);
 
-     c_e = clock() - c_s;     
-     printf("count = %d\n,    time = %i\n", iter, c_e);
+        c_e = clock() - c_s;
+        printf("count = %d\ntime = %i\n", iter, c_e);
 }
 
-
-void *potok(void *param) 
+/* Number iteration function.
+ * Used to call in a stream
+ * 
+ */
+void *stream_iter(void *param) 
 {
-     for(int i; i < 10000000; i++){
-          iter++;
-     }
+        for (int i; i < 10000000; i++) { // constant - given in the task, used for comparison
+                iter++;
+        }
 
-     pthread_exit(0);
+        pthread_exit(0);
 }
