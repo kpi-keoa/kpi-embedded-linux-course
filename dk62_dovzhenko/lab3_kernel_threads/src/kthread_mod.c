b7928027ba3325951bc74d4070b877683da95c29
Dana Dovzhenko <d.dovgenko@gmail.com>
2019-09-23 17:54:13 +0300

    fixed lab3

--- dk62_dovzhenko/lab3_kernel_threads/src/kthread_mod.c
+++ dk62_dovzhenko/lab3_kernel_threads/src/kthread_mod.c
@@ -22,40 +22,37 @@
 
 MODULE_DESCRIPTION("Basic module demo: kthread, list, atomic");
 MODULE_AUTHOR("AlexOstrianko; maksimo0; DovzhenkoD");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 MODULE_LICENSE("Dual MIT/GPL");// this affects the kernel behavior
 
 static int incr_val;
 static int kth_num;
-
-int *cnt;
-atomic_t *arg;
-
 module_param(incr_val, int, 0);
-MODULE_PARM_DESC(incr_val, "Value by which 'cnt' variable is incremented by threads");
 module_param(kth_num, int, 0);
 MODULE_PARM_DESC(kth_num, "Number of threads to create");
+MODULE_PARM_DESC(incr_val, "Value by which 'cnt' variable is incremented by threads");
 
-typedef struct struct_for_res_n {
+int *cnt = NULL;
+atomic_t *arg = NULL;
+
+struct struct_for_res {
 	struct list_head list;
 	int num;
-}struct_for_res;
+};
 	
-struct task_struct **kth_ptr;
-struct_for_res *struct_ptr;
-struct_for_res main_struct;
+struct struct_for_res *struct_ptr = NULL;
+struct struct_for_res main_struct;
 
-static atomic_t *new_lock(void)
+static void *new_lock(void)
 {
-	atomic_t *ptr = (atomic_t *)kmalloc(sizeof(atomic_t), GFP_ATOMIC);
+	atomic_t *ptr = kmalloc(sizeof *ptr, GFP_ATOMIC);
 	arch_atomic_set(ptr, 0);
 	return ptr;
 }
 
 static void lock(atomic_t *arg)
 {
-	do {
-	} while (arch_atomic_cmpxchg(arg, 0, 1) == 1);
+	while (arch_atomic_cmpxchg(arg, 0, 1) == 1);
 }
 
 static void unlock(atomic_t *arg)
@@ -63,25 +60,19 @@ static void unlock(atomic_t *arg)
 	arch_atomic_set(arg, 0);
 }
 
-static void free_lock(atomic_t *ptr)
-{
-	kfree(ptr);
-}
-
 int thread_func(void *data)
 {
-	int i;
-	for (i = 0; i < incr_val; i++) {
+	int *val = data;
+	for (int i = 0; i < incr_val; i++) {
 		lock(arg);
-		*(int *)data += 1;
+		*val += 1;
 		unlock(arg);
 		schedule();
 	}
-	struct_ptr = (struct_for_res *)kmalloc(sizeof(struct_for_res), 
-	GFP_KERNEL);
+	struct_ptr = kmalloc(sizeof *struct_ptr, GFP_KERNEL);
 	if (!struct_ptr) 
 		goto struct_Error;
-	struct_ptr->num = *(int *)data;
+	struct_ptr->num = *val;
 	list_add(&struct_ptr->list, &main_struct.list);
 	printk(KERN_INFO "All calculated #%d\n", struct_ptr->num);
 	do_exit(1);
@@ -93,48 +84,54 @@ int thread_func(void *data)
 
 static int __init kmod_init(void)
 {
+	struct task_struct **kth_ptr = NULL;
 	INIT_LIST_HEAD(&main_struct.list);
 	arg = new_lock();
-	if(!arg) 
+	if (!arg) 
 		goto lock_Error;
-	cnt = (int *)kmalloc(sizeof(int), GFP_KERNEL);
+	cnt = kmalloc(sizeof *cnt, GFP_KERNEL);
 	if (!cnt) 
 		goto cnt_Error;
 	*cnt = 0;
-	kth_ptr = (struct task_struct **)kmalloc(sizeof(struct task_struct *) * 
-	kth_num, GFP_KERNEL);
+	kth_ptr = kmalloc(sizeof *kth_ptr * kth_num, GFP_KERNEL);
 	if (!kth_ptr) 
 		goto kth_Error;	
-	int i;
-	for (i = 0; i < kth_num; i++) {
-		kth_ptr[i] = kthread_run(thread_func, (void *)cnt, "thread_%d", 
-		i);
+	for (int i = 0; i < kth_num; i++) {
+		kth_ptr[i] = kthread_run(&thread_func, (void *)cnt, "thread_%d", 
+			i);
 	}
 	return 0;
 	
 	
-	lock_Error: printk(KERN_ERR "Kmalloc didn`t allocate memory for 'lock'!\n");
-	cnt_Error: printk(KERN_ERR "Kmalloc didn`t allocate memory for cnt variable!\n"); 
-	kth_Error:  printk(KERN_ERR "Kmalloc didn`t allocate memory for threads pointer!\n"); 
-	return ENOMEM;
+	kth_Error:
+		kfree(kth_ptr);
+		kth_ptr = NULL;
+	cnt_Error:
+		kfree(cnt);
+		cnt = NULL;
+	lock_Error:  
+		printk(KERN_ERR "Kmalloc didn`t allocate memory!\n"); 
+	return 0;
 }
 
 static void __exit kmod_exit(void)
-{ 
-	printk(KERN_NOTICE "Counter value is #%d!\n", *cnt);
+{
+	if (cnt) {
+		printk(KERN_NOTICE "Counter value is #%d!\n", *cnt);
+		kfree(cnt);
+	} else 
+		printk(KERN_NOTICE "Counter is not available!\n");
 	struct list_head *head_ptr;
 	struct list_head *tmp;	
 	list_for_each_safe(head_ptr, tmp, &(main_struct.list)) {
-		struct_ptr = list_entry(head_ptr, struct_for_res, list);
+		struct_ptr = list_entry(head_ptr, struct struct_for_res, list);
 		printk(KERN_NOTICE "Deleting list element #%d!\n", 
-		struct_ptr->num);
+			struct_ptr->num);
 		list_del(head_ptr);
 		kfree(struct_ptr);		
 	}
-	if(cnt)
-		kfree(cnt);
-	if(arg)
-		free_lock(arg);
+	if (arg)
+		kfree(arg);
 	printk(KERN_NOTICE "Module unloaded!\n" ); 
 }
 
