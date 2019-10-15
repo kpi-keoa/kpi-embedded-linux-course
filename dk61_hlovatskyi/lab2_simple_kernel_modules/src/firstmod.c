a11f03388daba4253a6df3b6a8527e475720b9d3
Dmitriy Hlovatskiy <d.glovatskiy@gmail.com>
2019-10-15 21:37:36 +0300

    fixed commit history

--- dk61_hlovatskyi/lab2_simple_kernel_modules/src/firstmod.c
+++ dk61_hlovatskyi/lab2_simple_kernel_modules/src/firstmod.c
@@ -1,6 +1,7 @@
 /**
  * based on
- * https://github.com/kpi-keoa/kpi-embedded-linux-course/blob/master/demo/lab1/firstmod.c
+ * https://github.com/kpi-keoa/kpi-embedded-linux-course/
+ * blob/master/demo/lab1/firstmod.c
  * by thodnev
  */
 
@@ -8,15 +9,15 @@
 #include <linux/kernel.h>	 // required for sysinfo
 #include <linux/init.h>		 // used by module_init, module_exit macros
 #include <linux/jiffies.h>	 // where jiffies and its helpers reside
-#include <linux/interrupt.h> // required by tasklets
+#include <linux/interrupt.h>	 // required by tasklets
 
 MODULE_DESCRIPTION("Basic module demo: init, deinit, printk, jiffies, tasklet");
-MODULE_AUTHOR("thodnev & bramory"); 
+MODULE_AUTHOR("bramory; thodnev"); 
 MODULE_VERSION("0.1");
 MODULE_LICENSE("DualMIT/GPL");	//it affects the kernel behavior
 
 static char *user = "$username";
-module_param(user, charp, S_IRUGO);
+module_param(user, charp, 0000);
 
 void tasklet_handler(unsigned long arg)
 {
@@ -28,7 +29,7 @@ DECLARE_TASKLET(my_tasklet, tasklet_handler, (unsigned long) NULL);
 static int __init firstmod_init(void)
 {
 	printk(KERN_INFO "Hello, %s!\nOur jiffies = %lu\n", user, jiffies);
-	tasklet_schedule(&my_tasklet); //waits for run
+	tasklet_schedule(&my_tasklet);
 	return 0;
 }
  
