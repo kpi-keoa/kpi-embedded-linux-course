e78c74abc4213438523174699ea9373f37a933a2
gxbzig <36753665+gxbzig@users.noreply.github.com>
2020-01-10 01:00:15 +0200

    lab2
    lab2

---
+++ dk61_sydorchuk/lab2_simplest_kernel_modules/inc/firstmod.h
@@ -0,0 +1,8 @@
+#ifndef FIRST_MOD_  
+#define FIRST_MOD_
+
+static void tasklet_handler(unsigned long jiffy_value);
+static int __init firstmod_init(void);
+static void __exit firstmod_exit(void);
+
+#endif
