7ff4ebdd83dfea94123e7a644bacd9f5a068f558
Dmitriy Hlovatskiy <d.glovatskiy@gmail.com>
2020-01-09 20:58:08 +0200

    add lab4

---
+++ dk61_hlovatskyi/lab4_delayed_work/README.rst
@@ -0,0 +1,78 @@
+===========================
+**Lab3: delayed_work**
+===========================
+
+**Description**
+
+In this work we need to implement own instance of the kernel module with 
+dynamically allocated kernel threads and the struct of the lists.
+
+
+
+**Explaining**
+
+Dynamically allocate memory for the threads, variable and lists. 
+This function that we neeed is declared in ``<linux/slab.h>:``
+``void * kmalloc(size_t size, int flags)``
+
+Later, when you no longer need the memory, do not forget to free it:
+``void kfree(const void *ptr)``
+
+
+The flags are broken up into three categories: action modifiers, zone modifiers, and types. 
+
+**Action modifiers** specify **how** the kernel is supposed **to allocate** the requested memory. In certain situations, only certain methods can be employed to allocate memory. For example, interrupt handlers must instruct the kernel not to sleep (because interrupt handlers cannot reschedule) in the course of allocating memory. 
+
+**Zone modifiers** specify **from where to allocate** memory. The kernel divides physical memory into multiple zones, each of which serves a different purpose. Zone modifiers specify from which of these zones to allocate. 
+
+**Type flags specify a combination of action and zone modifiers** as needed by a certain type of memory allocation. Type flags simplify specifying numerous modifiers; instead, **you generally specify just one type flag**. The ``GFP_KERNEL`` is a type flag, which is used for code in process context inside the kernel. 
+
+
+**Implementing**
+
+We are wrote a code to process syncronized threads that have an access to the global variable and list to work with. After processing results are save to the nodes of the lists. 
+To do that in a right way we need to use any syncronizing tools.
+Spinlocks were implemented with atomic variables and funcions declared in <asm/atomic.h> which guarantee that only one thread can have an access to shared resources.     
+
+**Module assembly and testing**
+
+Use ``make`` to build a module
+The main parts of the code are:
+
+thread function - calculates the value with ``N_THREADS`` of threads by adding an ``increment`` on each step
+spinlocks - provide correct results by blocking access to the variable for another threads, which are not use variable at that exact time 
+lists -  contain data inside our predeclared structure 
+
+We can declare the value of the:
+``N_THREADS`` - amount of the threads to create
+``increment`` - step for incrementing
+``N_TIMES`` -  amount of steps to do adding
+
+**Results**
+
+You can see what we have without using spinlocks we have troubles :
+
+.. image:: img/no_spinlock.png
+
+But with spinlocks result is correct:
+
+.. image:: img/spinlock.png
+
+
+Just as in user-space, be careful to balance your allocations with your deallocations to prevent memory leaks and other bugs. Note, calling ``kfree(NULL)`` is explicitly checked for and safe.
+
+
+Used sources:
+
+<http://books.gigatux.nl/mirror/kerneldevelopment/0672327201/ch11lev1sec4.html>
+<https://www.ibm.com/developerworks/ru/library/l-linux_kernel_71/>
+<https://github.com/torvalds/linux/blob/master/include/linux/kthread.h>
+
+
+**Contacts**
+
+You can also contact me:
+
+**Telegram:** @Bramory <https://tg.me/Bramory>
+
+**E-mail** <glovatskiy.dmitriy@gmail.com>
---
+++ dk61_hlovatskyi/lab4_delayed_work/src/mod.c
@@ -0,0 +1,189 @@
+#include <linux/module.h>	// required by all modules
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>	// required for sysinfo
+#include <linux/init.h>		// used by module_init, module_exit macros
+#include <linux/jiffies.h>	// where jiffies and its helpers reside
+#include <linux/interrupt.h>
+#include <linux/kthread.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+#include <asm/atomic.h>
+#include <linux/timer.h>
+#include <linux/types.h>	
+#include <linux/completion.h>
+#include <linux/workqueue.h>
+
+MODULE_DESCRIPTION("Kernel delay");
+MODULE_AUTHOR("bramory");
+MODULE_VERSION("0.1");
+MODULE_LICENSE("Dual MIT/GPL");		// this affects the kernel behavior
+
+int num_of_threads = 2;
+static struct task_struct **thread = NULL;
+
+void print_and_erase_list(struct list_head *print_list);
+
+//declaration for complition
+static DECLARE_COMPLETION(comp_timer);
+static DECLARE_COMPLETION(comp_wq);
+
+//declaration for list
+static LIST_HEAD(list_timer_thread);
+static LIST_HEAD(list_wq_thread);
+struct long_node
+{
+	struct list_head list;
+	long data;
+};
+
+//declaration for workqueue
+void wq_fun(struct work_struct *work);
+DECLARE_DELAYED_WORK(wq_task, wq_fun);
+
+static struct timer_list my_timer;
+
+void timer_fun(struct timer_list *t)
+{
+    printk(KERN_INFO "TIMER %ld, %d\n", jiffies, jiffies%11);
+    
+    if((jiffies%11) == 0)
+    {
+    	printk(KERN_INFO "TIMER GOT TRUE JIFFIES %ld, %d\n", jiffies, jiffies%11);
+    	complete(&comp_timer);
+    }
+    else
+    {
+       	struct long_node * node = kmalloc(sizeof(struct long_node), GFP_KERNEL);
+       	if(NULL == node)
+       	{
+       		printk(KERN_ERR "TIMER FUN: ERROR allocting memory\n");
+       		complete(&comp_timer);
+    		print_and_erase_list(&list_timer_thread);
+       	}
+       	else
+       	{
+       		node->data = jiffies;
+			list_add(&node->list, &list_timer_thread);
+		
+    		mod_timer(&my_timer, jiffies + 17);
+       	}
+    }
+}
+
+int thread_timer(void *arg)
+{
+	timer_setup(&my_timer, timer_fun, 0);
+  	mod_timer(&my_timer, jiffies + msecs_to_jiffies(10000));
+	
+	wait_for_completion(&comp_timer);
+	do_exit(0);
+}
+
+void wq_fun(struct work_struct *work)
+{
+    printk(KERN_INFO "WQ %ld, %d\n", jiffies, jiffies%11);
+    
+    if((jiffies%11) == 0)
+    {
+    	printk(KERN_INFO "WQ GOT TRUE JIFFIES %ld, %d\n", jiffies, jiffies%11);
+    	complete(&comp_wq);
+    }
+    else
+    {
+       	struct long_node * node = kmalloc(sizeof(struct long_node), GFP_KERNEL);
+       	if(NULL == node)
+       	{
+       		printk(KERN_ERR "WQ FUN: ERROR allocting memory\n");
+       		complete(&comp_wq);
+   	 		print_and_erase_list(&list_wq_thread);
+       	}
+       	else
+       	{
+       		node->data = jiffies;
+			list_add(&node->list, &list_wq_thread);
+		
+    		schedule_delayed_work(&wq_task, 17);
+       	}
+    } 
+    
+}
+
+int thread_wq(void *arg)
+{
+	schedule_delayed_work(&wq_task, msecs_to_jiffies(1000));
+	wait_for_completion(&comp_wq);
+	do_exit(0);
+}
+
+void print_and_erase_list(struct list_head *print_list)
+{
+	if(NULL == print_list)
+    {
+    	printk(KERN_ERR "Cant print NULL list\n");
+      	return;
+    }
+	
+	struct list_head *current_pos = NULL;
+	struct list_head *tmp = NULL;
+	struct long_node * current_node = NULL;
+	list_for_each_safe(current_pos, tmp, print_list) {
+         
+         current_node = list_entry(current_pos, struct long_node, list);
+         
+         printk ("List element = %ld\n" , current_node->data);
+         
+         list_del(current_pos);
+         kfree(current_node);
+    }
+}
+
+static int __init kernel_thread_init(void)
+{	
+	thread = kmalloc(sizeof(struct task_struct **)*num_of_threads, GFP_KERNEL);
+	if(NULL == thread)
+    {
+    	printk(KERN_ERR "MAIN THREAD: Cant allocate memory for threads\n");
+      	return -1;
+    }
+    
+	thread[0] = kthread_run(thread_timer, NULL, "thread_timer");
+	
+	if(ERR_PTR(-ENOMEM) == thread[0])
+	printk(KERN_ERR "MAIN THREAD: thread_timer have not started\n");
+	
+	thread[1] = kthread_run(thread_wq, NULL, "thread_wq");
+	
+	if(ERR_PTR(-ENOMEM) == thread[0])
+	printk(KERN_ERR "MAIN THREAD: thread_wq have not started\n");
+	
+	
+	printk(KERN_INFO "Main thread!\n");
+	
+	return 0;
+}
+
+
+
+static void __exit kernel_thread_exit(void)
+{
+	printk(KERN_INFO "Closing the module\n");
+	
+	printk(KERN_INFO "Timer list\n");
+	print_and_erase_list(&list_timer_thread);
+	
+	printk(KERN_INFO "WQ list\n");
+	print_and_erase_list(&list_wq_thread);
+	
+	complete(&comp_timer);
+	complete(&comp_wq);
+	
+	del_timer(&my_timer);
+	cancel_delayed_work(&wq_task);
+	
+	kfree(thread);
+	
+	printk(KERN_INFO "See you later aligator!\n");
+}
+ 
+module_init(kernel_thread_init);
+module_exit(kernel_thread_exit);
