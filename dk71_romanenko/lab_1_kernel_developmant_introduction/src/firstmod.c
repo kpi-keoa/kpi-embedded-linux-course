1e557689d4f69cbd7fb5ab289028bafe19068336
Slavaromanenko3424 <gangstar7778@gmail.com>
2020-09-28 16:22:21 +0300

    added lab1

---
+++ dk71_romanenko/lab_1_kernel_developmant_introduction/src/firstmod.c
@@ -0,0 +1,48 @@
+/**
+ * based on
+ * https://github.com/kpi-keoa/kpi-embedded-linux-course/
+ * blob/master/demo/lab1/firstmod.c
+ * by thodnev
+ */
+
+#include <linux/module.h>	 // required by all modules
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>	 // required for sysinfo
+#include <linux/init.h>		 // used by module_init, module_exit macros
+#include <linux/jiffies.h>	 // where jiffies and its helpers reside
+
+
+MODULE_DESCRIPTION("Basic module demo: init, deinit, printk, jiffies");
+MODULE_AUTHOR("cartman; thodnev"); 
+MODULE_VERSION("0.1");
+MODULE_LICENSE("DualMIT/GPL");	//it affects the kernel behavior
+
+static char *user = NULL;
+static int beginning;
+
+module_param(user, charp, 0000);
+
+
+static int __init firstmod_init(void)
+{
+	beginning = jiffies;
+
+    if (!user) {
+        printk(KERN_WARNING "Username was not entered into the command line\n");
+        printk(KERN_INFO "Hello, $username!\n");
+
+    } else {
+        printk(KERN_INFO "Hello,  %s!\njiffies = %lu\n", user, jiffies);
+    }
+
+}
+ 
+static void __exit firstmod_exit(void)
+{
+	printk(KERN_INFO "Long live the Kernel! =)\n");
+	printk(KERN_INFO "Working time is - %u seconds \n",
+	 		jiffies_delta_to_ms(jiffies - beginning) / 1000);     
+}
+ 
+module_init(firstmod_init);
+module_exit(firstmod_exit);
