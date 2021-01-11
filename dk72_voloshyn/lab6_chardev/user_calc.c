3bee2b7efaab46497c991e660fd9693a560c9eb5
sad <avoloshyn111@gmail.com>
2021-01-11 11:22:00 +0200

    fix 6lab

--- dk72_voloshyn/lab6_chardev/user_calc.c
+++ dk72_voloshyn/lab6_chardev/user_calc.c
@@ -12,43 +12,43 @@
 
 int main()
 {
-	int num;
-	int fd;
-	int32_t value, number1, number2, output, operation;
+    int num;
+    int fd;
+    int32_t value, number1, number2, output, operation;
 
-	fd = open("/dev/sad_device", O_RDWR);
-	if (fd < 0) {
-		printf("Cannot open device file ...\n");
-		return 0;
-	}
+    fd = open("/dev/sad_device", O_RDWR);
+    if (fd < 0) {
+        printf("Cannot open device file ...\n");
+        return 0;
+    }
 
-	printf("Operation to perform\n\n");
-	printf("\
-		1. Add \n \
-		2. Subtract \n \
-		3. Multiply \n \
-		4. Divide \n");
+    printf("Operation to perform\n\n");
+    printf("\
+        1. Add \n \
+        2. Subtract \n \
+        3. Multiply \n \
+        4. Divide \n");
 
-	scanf("%d", &num);   
-	if(num > 4 && num < 1)
-	{
-		printf("Enter between 1 and 4");
-		return 0;
-	}
-	ioctl(fd, WR_VALUE, (int32_t*) &num);
+    scanf("%d", &num);   
+    if(num > 4 && num < 1)
+    {
+        printf("Enter between 1 and 4");
+        return 0;
+    }
+    ioctl(fd, WR_VALUE, (int32_t*) &num);
 
-	printf("Enter first value :");
-	scanf("%d", &number1);
+    printf("Enter first value :");
+    scanf("%d", &number1);
 
-	printf("Enter second value :");
-	scanf("%d", &number2);
+    printf("Enter second value :");
+    scanf("%d", &number2);
 
-	ioctl(fd, WR_VALUE, (int32_t*) &number1);
-	ioctl(fd, WR_VALUE, (int32_t*) &number2);
+    ioctl(fd, WR_VALUE, (int32_t*) &number1);
+    ioctl(fd, WR_VALUE, (int32_t*) &number2);
 
-	ioctl(fd, RD_VALUE, (int32_t*)&value);
-	printf("value is %d\n", value);
+    ioctl(fd, RD_VALUE, (int32_t*)&value);
+    printf("value is %d\n", value);
 
-	close(fd);
+    close(fd);
 }  
 
